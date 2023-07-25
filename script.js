9978981be397ac826d05cb62ac18d155f65ede42
Aditya Harde <110459473+Aditya049823@users.noreply.github.com>
2023-07-25 09:08:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const pElement=document.getElementById("timer");
+const current=new Date();
+pElement.innerText=current.toLocaleDateString()+", "+ current.toLocaleTimeString();
+
